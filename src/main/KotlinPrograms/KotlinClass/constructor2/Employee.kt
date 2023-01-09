f6ad3b6495336fda8183cd0c830671867ad56658
Cw-Sandeep <sandeep@cloudwalker.tv>
2023-01-09 18:17:37 +0530

    primary, secondary and third constructor added

--- src/main/KotlinPrograms/KotlinClass/constructor2/Employee.kt
+++ src/main/KotlinPrograms/KotlinClass/constructor2/Employee.kt
@@ -4,10 +4,13 @@ package KotlinClass.constructor2
 class Employee (firstName: String, lastName: String, isPlatinum: Boolean)  {
 
     //assume we know employee is not platinum
+
+    //secondary Constructor
     constructor(firstName: String , lastName: String) : this(firstName, lastName,false) {
         println("IsPlatinum is False by Default")
     }
 
+    //third Constructor
     constructor(firstName: String): this(firstName, "unknown") {
         println("Third constructor")
 
